aa9d7635f461da47a90cab32dd314f54d523bc36
tiqwab <tiqwab.ch90@gmail.com>
2022-03-08 12:49:28 +0900

    Add KeyManager with basic interface

---
+++ src/key_manager.rs
@@ -0,0 +1,60 @@
+use anyhow::Result;
+use rand::rngs::OsRng;
+use rsa::pkcs1::ToRsaPublicKey;
+use rsa::pkcs8::ToPrivateKey;
+use rsa::{Hash, PaddingScheme, PublicKey, RsaPrivateKey, RsaPublicKey};
+use sha2::{Digest, Sha256};
+
+struct KeyManager {
+    private_key: RsaPrivateKey,
+    public_key: RsaPublicKey,
+    rng: OsRng,
+}
+
+impl KeyManager {
+    pub fn new() -> Result<KeyManager> {
+        let mut rng = OsRng;
+        let bits = 2048;
+        let private_key = RsaPrivateKey::new(&mut rng, bits)?;
+        let public_key = RsaPublicKey::from(&private_key);
+        Ok(KeyManager {
+            private_key,
+            public_key,
+            rng,
+        })
+    }
+
+    pub fn encrypt(&mut self, data: &[u8]) -> Result<Vec<u8>> {
+        let padding = PaddingScheme::new_pkcs1v15_encrypt();
+        let encrypted = self.public_key.encrypt(&mut self.rng, padding, data)?;
+        Ok(encrypted)
+    }
+
+    pub fn decrypt(&self, data: &[u8]) -> Result<Vec<u8>> {
+        let padding = PaddingScheme::new_pkcs1v15_encrypt();
+        let decrypted = self.private_key.decrypt(padding, data)?;
+        Ok(decrypted)
+    }
+
+    pub fn sign(&mut self, data: &[u8]) -> Result<Vec<u8>> {
+        let mut hasher = Sha256::new();
+        hasher.update(data);
+        let hashed = hasher.finalize();
+
+        let padding = PaddingScheme::PKCS1v15Sign {
+            hash: Some(Hash::SHA2_256),
+        };
+        let signed = self.private_key.sign(padding, &hashed)?;
+
+        Ok(signed)
+    }
+
+    /// Returns Ok(()) if verification succeeds.
+    pub fn verify_signature(&mut self, hashed: &[u8], signature: &[u8]) -> Result<()> {
+        let padding = PaddingScheme::PKCS1v15Sign {
+            hash: Some(Hash::SHA2_256),
+        };
+        self.public_key.verify(padding, hashed, signature)?;
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod blockchain;
 pub mod client_core;
 mod connection_manager_core;
 mod connection_manager_edge;
+mod key_manager;
 pub mod message;
 pub mod server_core;
